18d10324adbd02846f11521aac42067d84713a75
Seyifunmi Gandonu <655453@student.inholland.nl>
2020-05-16 18:02:54 +0200

    ChaeauModel Added

---
+++ ChapeauProject1.4/ChapeauModel/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    public class Login
+    {
+        public int employeeID { get; set; }
+        public int loginCode { get; set; }
+    }
+}
---
+++ ChapeauProject1.4/ChapeauModel/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    public class Order
+    {
+        public int OrderID;
+        public int TableNumber;
+        public int EmployeeID;
+        public string EmployeeName;
+        public int OrderDate;
+        public string Item;
+        public int Amount;
+        public string OrderStatus;
+
+        public void CreateOrder()
+        {
+
+        }
+
+        public void CreateItem()
+        {
+
+        }
+
+        public void AddItem()
+        {
+
+        }
+
+        public void DeleteItem()
+        {
+
+        }
+    }
+}
